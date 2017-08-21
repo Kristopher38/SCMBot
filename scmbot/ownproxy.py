36e37e96afafb5fed65abf85e581152e39f73333
Kristopher38 <Kristopher38@wp.pl>
2017-08-21 16:01:09 +0200

    Automatic commit

--- scmbot/ownproxy.py
+++ scmbot/ownproxy.py
@@ -1,11 +1,14 @@
 from .proxyfinder import ProxyFinder
 from scrapy import signals
+from scrapy.exceptions import CloseSpider
 from proxybroker import Proxy
 from .proxy import ProxyExtended
 from datetime import datetime, timedelta
 import random
 import pdb
 import logging
+import time
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -17,8 +20,8 @@ class OwnProxy(object):
 		self.proxy_types = crawler.settings.get('PROXY_TYPES', ['HTTP'])
 		self.hold_time = crawler.settings.get('PROXY_HOLD_TIME', 30)
 		self.min_hold_time = crawler.settings.get('MIN_PROXY_HOLD_TIME', 2)
+		#self.retry_words = crawler.settings.get('RETRY_WORDS', [])
 		self.finder = ProxyFinder(types=self.proxy_types, limit=self.limit)
-		self.proxies = self.finder.proxies
 		
 	@classmethod
 	def from_crawler(cls, crawler):
@@ -28,65 +31,98 @@ class OwnProxy(object):
 		return o
 		
 	def process_request(self, request, spider):
-		random_proxy = self._get_random_proxy()
-		random_proxy.stat['requests'] += 1
-		request.meta['proxy'] = "http://%s:%s" % (random_proxy.host, random_proxy.port)
-		request.meta['proxy_obj'] = random_proxy
+		try:
+			random_proxy = self._get_random_proxy()
+		except NoLeftInPoolError as e:
+			raise CloseSpider(e.__str__())
+		else:
+			random_proxy.stat['requests'] += 1
+			request.meta['proxy'] = "http://%s:%s" % (random_proxy.host, random_proxy.port)
+			request.meta['proxy_obj'] = random_proxy
 			
 	def process_response(self, request, response, spider):
 		if 'proxy_obj' in request.meta:
 			proxy = request.meta['proxy_obj']
 			proxy._runtimes.append(request.meta['download_latency'])
 			proxy.stat['status'][response.status] += 1
+			proxy.in_use = False
+			
 			if response.status != 200:
-				if response.status == 407 and self.proxies.count(proxy) > 0:	# Proxy authentication required
-					logger.debug("Removing proxy requring authentication %s", proxy)
-					self.proxies.remove(proxy)
 				logger.debug("(Failed request) Proxy used for %s was %s", request.url, request.meta['proxy'])
-				if response.status == 429:
+				if response.status in [302, 407] and self.finder.proxies.count(proxy) > 0:	# Proxy authentication required or redirection to info about restricted access
+					logger.debug("Removing proxy requring authentication or restricting access (code %d): %s", response.status, proxy)
+					self.finder.proxies.remove(proxy)
+				if response.status in [429, 503]:
 					logger.debug("Putting proxy %s on hold for %s seconds", proxy, str(self.hold_time))
 					proxy.hold_until = datetime.now() + timedelta(seconds=self.hold_time)
 			else:
 				proxy.hold_until = datetime.now() + timedelta(seconds=self.min_hold_time)
 				logger.debug("Proxy used for %s was %s", request.url, request.meta['proxy'])
-			proxy.in_use = False
+				# if not self._is_valid(response.text):
+					# if self.finder.proxies.count(proxy) > 0:
+						# logger.debug("Removing proxy restricting access %s", proxy)
+						# self.finder.proxies.remove(proxy)
+					# logger.debug("Retrying denied request made with proxy %s" % request.meta['proxy_obj'] if 'proxy_obj' in request.meta else None)
+					# retryreq = request.copy()
+					# retryreq.dont_filter = True
+					# return retryreq
 		return response
 	
 	def process_exception(self, request, exception, spider):
 		if 'proxy_obj' in request.meta:
 			proxy = request.meta['proxy_obj']
 			proxy.stat['errors'][exception.__class__] += 1
-			if sum(proxy.stat['errors'].values()) >= self.max_errors and self.proxies.count(proxy) > 0:
+			if sum(proxy.stat['errors'].values()) >= self.max_errors and self.finder.proxies.count(proxy) > 0:
 				logger.debug("Removing proxy exceeding max error count %s", proxy)
-				self.proxies.remove(proxy)
+				self.finder.proxies.remove(proxy)
+			logger.debug("Putting proxy %s on hold for %s seconds", proxy, str(self.hold_time))
+			proxy.hold_until = datetime.now() + timedelta(seconds=self.hold_time)
 			proxy.in_use = False
 		
 	def spider_opened(self, spider):
 		self.finder.start()
-		while len(self.proxies) < self.init_proxies:
+		while len(self.finder.proxies) < self.init_proxies:
 			self.finder.wait_for_proxy()
 		
 	def spider_closed(self, spider):
 		logger.info("Dumping proxy stats:")
-		for proxy in self.proxies:
-			logger.debug("%s %s Average runtime: %s Error rate: %s", proxy, proxy.stat, proxy.avg_resp_time, proxy.error_rate)
+		for proxy in self.finder.proxies:
+			logger.info("%s %s Average runtime: %s Error rate: %s", proxy, proxy.stat, proxy.avg_resp_time, proxy.error_rate)
 		self.finder.stop()
 		
 	def _get_random_proxy(self):
 		self.finder.update_proxies()
 		
-		if len(self.proxies) > 0:
-			for i, proxy in enumerate(self.proxies):
+		if len(self.finder.proxies) > 0:
+			for i, proxy in enumerate(self.finder.proxies):
 				if not isinstance(proxy, ProxyExtended):
-					self.proxies[i] = ProxyExtended(proxy)
-			
-			now = datetime.now()
-			self.proxies = sorted(self.proxies, key=lambda x: x.avg_resp_time, reverse=False)
-			for proxy in filter(lambda x: x.hold_until < now and x.in_use == False, self.proxies): # allow only if past time of holding to
-				proxy.last_used = now
-				proxy.in_use = True
-				return proxy
-			else:
-				raise RuntimeError("All proxies are on hold")
+					self.finder.proxies[i] = ProxyExtended(proxy)
 		else:
-			raise RuntimeError("No proxies left in the proxy pool")
+			raise NoLeftInPoolError("No proxies left in the proxy pool")
+
+		now = datetime.now()
+		self.finder.proxies = sorted(self.finder.proxies, key=lambda x: x.avg_resp_time, reverse=False)
+
+		for proxy in filter(lambda x: x.hold_until < now and x.in_use == False, self.finder.proxies): # allow only if past time of holding to
+			proxy.last_used = now
+			proxy.in_use = True
+			return proxy
+		else:
+			return min(self.finder.proxies, key=lambda x: x.hold_until)
+			#raise OnHoldError("All proxies are on hold")
+		
+	# def _is_valid(self, response_text):
+		# try:
+			# json.loads(response_text)
+		# except ValueError:
+			# text = response_text.lower()
+			# for word in self.retry_words:
+				# if word in text:
+					# return False
+			# return True
+		# else:
+			# return True
+
+class NoLeftInPoolError(RuntimeError):
+	pass
+	
--- scmbot/spiders/itemcrawler.py
+++ scmbot/spiders/itemcrawler.py
@@ -10,10 +10,28 @@ class ItemcrawlerSpider(scrapy.Spider):
 	name = "itemcrawler"
 	
 	allowed_domains = ['steamcommunity.com']
+	# custom_settings = {
+		# 'RETRY_HTTP_CODES': [400, 403, 404, 407, 408, 429, 500, 502, 503, 504],
+		# 'RETRY_TIMES': 10,
+		# 'DOWNLOADER_MIDDLEWARES': {
+			# 'scmbot.ownproxy.OwnProxy': 555,
+			# 'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 556,
+			# 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+			# 'scrapy.downloadermiddlewares.robotstxt.RobotsTxtMiddleware': None
+		# },
+		# 'DOWNLOAD_TIMEOUT': 15,
+		# 'RANDOM_UA_PER_PROXY': True,
+		# 'PROXY_LIMIT': 500,
+		# 'MIN_PROXY_INIT': 100,
+		# 'PROXY_HOLD_TIME': 60,
+		# 'PROXY_TYPES': [('HTTP', ('Anonymous', 'High'))],
+		# 'CONCURRENT_REQUESTS': 50
+	# }
 	custom_settings = {
-		'RETRY_HTTP_CODES': [400, 403, 404, 407, 408, 429, 500, 502, 503, 504],
+		'RETRY_HTTP_CODES': [302, 400, 403, 404, 407, 408, 429, 500, 502, 503, 504],
 		'RETRY_TIMES': 10,
 		'DOWNLOADER_MIDDLEWARES': {
+			#'scmbot.retrydenied.RetryDenied': 554,
 			'scmbot.ownproxy.OwnProxy': 555,
 			'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 556,
 			'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
@@ -21,11 +39,14 @@ class ItemcrawlerSpider(scrapy.Spider):
 		},
 		'DOWNLOAD_TIMEOUT': 15,
 		'RANDOM_UA_PER_PROXY': True,
-		'PROXY_LIMIT': 500,
-		'MIN_PROXY_INIT': 100,
-		'PROXY_HOLD_TIME': 60,
+		'PROXY_LIMIT': 75,
+		'MIN_PROXY_INIT': 25,
+		'PROXY_HOLD_TIME': 30,
 		'PROXY_TYPES': [('HTTP', ('Anonymous', 'High'))],
-		'CONCURRENT_REQUESTS': 50
+		'CONCURRENT_REQUESTS': 25,
+		'DUPEFILTER_DEBUG': True,
+		#'RETRY_WORDS': ['restricted', 'blocked'],
+		'REDIRECT_ENABLED': False
 	}
 	
 	links = {
@@ -33,7 +54,7 @@ class ItemcrawlerSpider(scrapy.Spider):
 		'priceoverview': 'http://steamcommunity.com/market/priceoverview/?appid={appid}&country={country}&currency={currency}&market_hash_name={market_hash_name}'
 	}
 	regexps = {
-		'search': 'market_listing_row_link.+href="(.+\/(.+?))\?',
+		'search': 'market_listing_row_link.+href="(.+\/(.+?))(?:\?|")',
 		'listings': 'var line1=(\[.+\]);'
 	}
 	
@@ -42,20 +63,57 @@ class ItemcrawlerSpider(scrapy.Spider):
 	profit_threshold = -9999999
 	price_perc_threshold = -999999
 	
+	file = 'kasztan.json'
+	
 	# TODO: Subclassed proxybroker with ability to queue jobs for grabbing proxies, pausing grabbing when done job, and queues in ProxyFinder to allow for that
-	# TODO: Refactor requests so parameters aren't hardcoded (todo: appid passing from appid in start_requests to appid in parse_search so it doesn't trigger internal server error)
 	
 	def start_requests(self):
-		for i in range(0, 500, 100):
-			url = self.links['search'].format(query="trading card", start=str(i), count=str(i+100), sort_column="quantity", sort_dir="desc", appid="753")
-			yield scrapy.Request(url=url, callback=self.parse_search)
-
+		crawl_settings_file = open(self.file)
+		crawl_settings = json.load(crawl_settings_file)
+		meta = {}
+		for crawl_item in crawl_settings:
+			if 'query' not in crawl_item:
+				crawl_item['query'] = ''
+			if 'appid' not in crawl_item:
+				crawl_item['appid'] = ''
+			if 'count' not in crawl_item:
+				crawl_item['count'] = None
+			if 'start' not in crawl_item:
+				crawl_item['start'] = 0
+			if 'sort_column' not in crawl_item:
+				crawl_item['sort_column'] = 'quantity'
+			if 'sort_dir' not in crawl_item:
+				crawl_item['sort_dir'] = 'desc'
+			
+			yield from self.get_search_link(crawl_item, crawl_item['count'])
+				
+	def get_search_link(self, crawl_item, count=None):
+		def _schedule_by_maxcount(response):
+			if self.is_json(response.text):
+				data = json.loads(response.text)
+				total_count = data['total_count']
+				crawl_item = response.meta['crawl_item']
+				yield from self.get_search_link(crawl_item, total_count)
+		
+		meta = {'crawl_item': crawl_item}
+		if count is None:
+			url = self.links['search'].format(query=crawl_item['query'], start=0, count=0, sort_column='',sort_dir='', appid=crawl_item['appid'])
+			yield scrapy.Request(url=url, callback=_schedule_by_maxcount, meta=meta)
+		else:
+			for i in range(0, count, 100):
+				url = self.links['search'].format(query=crawl_item['query'], 
+													  start=str(i), 
+													  count=str(100 if count - i >= 100 else count - i), 
+													  sort_column=crawl_item['sort_column'], 
+													  sort_dir=crawl_item['sort_dir'], 
+													  appid=crawl_item['appid'])
+				yield scrapy.Request(url=url, callback=self.parse_search, meta=meta)
+	
 	def parse_search(self, response):
 		if self.is_json(response.text):
-			#formatted = response.text.decode('string_escape').replace("\/", "/")
 			data = json.loads(response.text)
 			for item_url, item_name in re.findall(self.regexps['search'], data['results_html']):
-				priceoverview_url = self.links['priceoverview'].format(appid="753", country="PL", currency="3", market_hash_name=item_name)
+				priceoverview_url = self.links['priceoverview'].format(appid=response.meta['crawl_item']['appid'], country="PL", currency="3", market_hash_name=item_name)
 				meta = {'item_url': item_url, 'item_name': item_name}
 				yield scrapy.Request(url=priceoverview_url, callback=self.parse_priceoverview, meta=meta)
 		
@@ -63,12 +121,18 @@ class ItemcrawlerSpider(scrapy.Spider):
 		if self.is_json(response.text):
 			data = json.loads(response.text)
 			if data['success']:
-				data['volume'] = int(data['volume'].replace(",",""))
-				if int(data['volume']) >= self.volume_threshold:
+				try:
+					data['volume'] = int(data['volume'].replace(",",""))
 					meta = {'volume': data['volume']}
-					yield scrapy.Request(url=response.meta['item_url'], callback=self.parse_item, meta=meta)
+				except KeyError:
+					meta = {'volume': 0}
+				finally:
+					if meta['volume'] >= self.volume_threshold:
+						yield scrapy.Request(url=response.meta['item_url'], callback=self.parse_item, meta=meta)
 			else:
-				yield response.request.copy()
+				retryreq = response.request.copy()
+				retryreq.dont_filter = True
+				yield retryreq
 		
 	def parse_item(self, response):
 		try:
